e37dc93f9f83d2ad3a47f50323231dc1c74d3afa
Weaselcore <42705062+Weaselcore@users.noreply.github.com>
2022-04-07 17:20:59 +1200

    Added conponents, refactored to latest router code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,32 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, NavLink, Route, Routes } from "react-router-dom";
 
-// import Home from "./pages/Home";
-// import SEPractice from "./pages/SE-Practice";
-// import SubmitArticle from "./pages/Submit-Article"; 
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article";
 // import NotFoundPage from "./pages/404";
 
-const App = () =>  {
-    return (
-        <Router>
-        <div>
-          <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
-            <ul className="header">
-                <li><a href = "/">Home</a></li>
-                <li><a href = "/SEPractice">Select the Practice</a></li>
-                <li><a href = "/SubmitArticle">Submit an Article</a></li>
-            </ul>
-          <div className="content">
-          </div>
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
+        <ul className="header">
+          <li><NavLink exact to="/">Home</NavLink></li>
+          <li><NavLink to="/SEPractice">Select the Practice</NavLink></li>
+          <li><NavLink to="/SubmitArticle">Submit an Article</NavLink></li>
+
+        </ul>
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={Home()} />
+            <Route path="/SEPractice" element={SEPractice()} />
+            <Route path="/SubmitArticle" element={SubmitArticle()} />
+          </Routes>
         </div>
-        </Router>
-    );
+      </div>
+    </Router>
+  );
 }
- 
+
 export default App;
